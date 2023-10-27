30109d84df8255ba4c4ac3a584b6a38156f8a578
odevpedro <pedrosschmidt2@gmail.com>
2023-10-27 16:39:04 -0300

    fix all examples

---
+++ src/BigO/Main.java
@@ -0,0 +1,46 @@
+package BigO;
+public class Main {
+
+    //Big O notation O(n)
+
+    /**
+
+     public static void printItems(int n){
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+            public static void main(String[] args){
+            printItems(10);
+            }
+
+     **/
+
+
+    //Big O notation O(n^2)
+
+/**
+        public static void printItems(int n) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.println(i + " " + j);
+                }
+            }
+        }
+        public static void main(String[] args){
+            printItems(10);
+        }
+    }
+ **/
+
+
+//Big O notation O(1)
+
+/**
+
+ public static void addItems(int n){
+      return n + n + n;
+  }
+  
+ **/
+
+}
